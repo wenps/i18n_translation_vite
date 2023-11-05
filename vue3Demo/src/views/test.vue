5aed205c89c975cb794ad4d6887e206cc6ce775f
文鹏山 <wenpengshan@wenpengshandeMacBook-Pro.local>
2023-11-05 10:42:15 +0800

    feat: vue3demorouter测试

---
+++ vue3Demo/src/views/test.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Author: xiaoshanwen
+ * @Date: 2023-08-09 11:48:25
+ * @LastEditTime: 2023-11-05 10:40:52
+ * @FilePath: /i18n_translation_vite/vue3Demo/src/views/test.vue
+-->
+<template>
+  <div>
+  const a = '小山' 
+  sadsadas, {{ a }}不是吧
+
+  啊这
+  </div>
+</template>
+<script lang="ts" setup>
+  // 验证是否会翻译注释
+  import { ref } from "vue"
+  const a = '小山' 
+  console.log(a);
+  const msg = ref('hello world')
+</script>

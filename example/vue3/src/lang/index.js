1404704d17fbddfaeaa0828c91ec3fd2c7c77e0d
文鹏山 <wenpengshan@wenpengshandeMacBook-Pro.local>
2023-11-20 14:09:15 +0800

    feat: Remove import logic for generated translated mjs files

--- example/vue3/src/lang/index.js
+++ example/vue3/src/lang/index.js
@@ -1,19 +1,16 @@
 /*
  * @Author: xiaoshanwen
  * @Date: 2023-10-12 13:08:36
- * @LastEditTime: 2023-11-11 17:48:50
+ * @LastEditTime: 2023-11-20 14:00:47
  * @FilePath: /i18n_translation_vite/example/vue3/src/lang/index.js
  */
 import '../../lang/index'
-import EN from '../../lang/en/index.mjs'
-import CN from '../../lang/zh-cn/index.mjs'
-import KO from '../../lang/ko/index.mjs'
-import JA from '../../lang/ja/index.mjs'
+import langJSON from '../../lang/index.json'
 const langMap = {
-    en: window?.lang?.en || EN,
-    zhcn: window?.lang?.zhcn || CN,
-    ko: window?.lang?.ko || KO,
-    ja: window?.lang?.ja || JA,
+    en: window?.lang?.en || _getJSONKey('en', langJSON),
+    zhcn: window?.lang?.zhcn || _getJSONKey('zhcn', langJSON),
+    ko: window?.lang?.ko || _getJSONKey('ko', langJSON),
+    ja: window?.lang?.ja || _getJSONKey('ja', langJSON),
 }
 const lang = window.localStorage.getItem('lang') || 'zhcn'
 window.$t.locale(langMap[lang], 'lang')

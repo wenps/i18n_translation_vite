75ff3f6a2dc4403800b5e2f93d9db78bf966c5ea
文鹏山 <wenpengshan@wenpengshandeMacBook-Pro.local>
2024-02-29 13:56:48 +0800

    feat:初始化webpack翻译插件

---
+++ webpackPluginsAutoI18n/src/index.ts
@@ -0,0 +1,9 @@
+/*
+ * @Author: xiaoshanwen
+ * @Date: 2024-02-29 13:33:53
+ * @LastEditTime: 2024-02-29 13:36:56
+ * @FilePath: /i18n_translation_vite/webpackPluginsAutoI18n/src/index.ts
+ */
+export default function webpackPluginsAutoI18n() {
+  
+}

dcb50fc232618843dbc54ca60b88869a9e9f2100
Caleb-Xu <noname1427968964@outlook.com>
2024-05-17 00:55:20 +0800

    feat: 翻译器基本实现

--- autoI18nPluginCore/src/utils/translate/translator/google.ts
+++ autoI18nPluginCore/src/utils/translate/translator/google.ts
@@ -1,1 +1,27 @@
-export const a = 1
+import { translate } from '@vitalets/google-translate-api'
+import { Translator } from '.'
+import tunnel from 'tunnel'
+
+export interface GoogleTranslatorOption {
+    proxyOption: tunnel.ProxyOptions
+}
+
+export class GoogleTranslator extends Translator {
+    constructor(option: GoogleTranslatorOption) {
+        super({
+            name: 'Google翻译',
+            fetchMethod: async (text, fromKey, toKey) => {
+                let data = await translate(text, {
+                    from: fromKey,
+                    to: toKey,
+                    fetchOptions: {
+                        agent: tunnel.httpsOverHttp({
+                            proxy: option.proxyOption
+                        })
+                    }
+                })
+                return data['text'] || ''
+            }
+        })
+    }
+}
--- autoI18nPluginCore/src/utils/translate/translator/index.ts
+++ autoI18nPluginCore/src/utils/translate/translator/index.ts
@@ -1,8 +1,33 @@
-export class Translator {
-    constructor() {}
+export interface TranslatorOption {
+    fetchMethod: (text: string, fromKey: string, toKey: string) => Promise<string>
+    name?: string
+}
 
-    translate(text: string, fromKey: string, toKey: string) {
-        // TODO
+export class Translator {
+    protected option: TranslatorOption
+
+    constructor(option: TranslatorOption) {
+        this.option = option
+    }
+
+    protected getErrorMessage(error: unknown) {
+        if (error instanceof Error) {
+            return error.message
+        } else {
+            return String(error)
+        }
+    }
+
+    async translate(text: string, fromKey: string, toKey: string) {
+        // TODO: 后续可以改造成节流
+        try {
+            return await this.option.fetchMethod(text, fromKey, toKey)
+        } catch (error) {
+            const name = this.option.name
+            console.error(
+                `翻译api${name ? `【${name}】` : ''}请求异常：${this.getErrorMessage(error)}`
+            )
+        }
     }
 }
 

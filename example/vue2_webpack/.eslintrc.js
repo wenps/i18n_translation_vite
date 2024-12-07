30149d354175729c475c6e9f4f481ee32a89e0ad
xiaoshanwen <xiaoshanwen@canway.net>
2024-12-07 16:03:02 +0800

    feat: 测试项目优化

--- example/vue2_webpack/.eslintrc.js
+++ example/vue2_webpack/.eslintrc.js
@@ -1,17 +1,25 @@
+/*
+ * @Date: 2024-12-07 15:17:43
+ * @LastEditors: xiaoshan
+ * @LastEditTime: 2024-12-07 15:28:02
+ * @FilePath: /i18n_translation_vite/example/vue2_webpack/.eslintrc.js
+ */
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
-  parserOptions: {
-    parser: '@babel/eslint-parser'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    root: true,
+    env: {
+        node: true
+    },
+    parser: 'vue-eslint-parser',
+    parserOptions: {
+        parser: '@typescript-eslint/parser',
+        sourceType: 'module'
+    },
+    overrides: [
+        {
+            files: ['*.vue'],
+            rules: {
+                indent: 'off'
+            }
+        }
+    ]
 }

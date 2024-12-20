eaafbc215741f7c1261d0dcfc838b9c1f2b68c83
weifanyang <1123642601@qq.com>
2024-12-20 21:55:58 +0800

    feat: update version

--- script/upload.js
+++ script/upload.js
@@ -25,107 +25,32 @@ const VersionTypeEnum = {
 
 const buildCmd = 'pnpm build'
 
-/**
- * 选择 core ，同时会自动打包 plugin
- * 选择 plugin ，需要自行选择打包
- */
 const run = async () => {
-    const type = await select({
-        name: 'type',
-        choices: [
-            { name: 'core', value: TypeEnum.CORE },
-            { name: 'plugin', value: TypeEnum.PLUGIN }
-        ],
-        message: '请选择要打包的类型：',
-        default: []
-    })
-
-    if (type === TypeEnum.CORE) {
-        console.log(chalk.green`\n开始打包 core 和 plugin\n`)
-
-        for (let key in TypeDirNameMap) {
-            shell.exec(`cd ${`packages/${TypeDirNameMap[key]}`} && ${buildCmd}`)
-        }
-        console.log(chalk.green`\n打包完成!\n`)
-
-        const versionType = await selectVersionType()
-
-        console.log(chalk.green`\n开始修改版本号\n`)
-
-        for (let key in TypeDirNameMap) {
-            await generateVersion(
-                versionType,
-                `../packages/${TypeDirNameMap[key]}/package.json`,
-                TypeDirNameMap[key]
-            )
-        }
-        console.log(chalk.green`\n修改完成!\n`)
-
-        commitCode()
-    } else {
-        const pluginType = await checkbox({
-            name: 'pluginType',
-            choices: [
-                { name: 'webpack', value: PluginTypeEnum.WEBPACK },
-                { name: 'vite', value: PluginTypeEnum.VITE }
-            ],
-            message: '请选择要打包的插件类型：',
-            default: []
-        })
-
-        console.log(chalk.green`\n开始打包 plugin\n`)
-
-        if (pluginType.includes(PluginTypeEnum.WEBPACK)) {
-            shell.exec(`cd ${`packages/${TypeDirNameMap[PluginTypeEnum.WEBPACK]}`} && ${buildCmd}`)
-        }
-
-        if (pluginType.includes(PluginTypeEnum.VITE)) {
-            shell.exec(`cd ${`packages/${TypeDirNameMap[PluginTypeEnum.VITE]}`} && ${buildCmd}`)
-        }
-
-        console.log(chalk.green`\n打包完成!\n`)
-
-        if (pluginType.includes(PluginTypeEnum.WEBPACK)) {
-            console.log(chalk.green`\n开始修改 webpack plugin 版本号`)
-
-            const versionType = await selectVersionType()
-            await generateVersion(
-                versionType,
-                `../packages/${TypeDirNameMap[PluginTypeEnum.WEBPACK]}/package.json`,
-                TypeDirNameMap[PluginTypeEnum.WEBPACK]
-            )
-        }
-
-        if (pluginType.includes(PluginTypeEnum.VITE)) {
-            console.log(chalk.green`\n开始修改 vite plugin 版本号`)
-            const versionType = await selectVersionType()
-            await generateVersion(
-                versionType,
-                `../packages/${TypeDirNameMap[PluginTypeEnum.VITE]}/package.json`,
-                TypeDirNameMap[PluginTypeEnum.VITE]
-            )
-        }
-
-        console.log(chalk.green`\n修改完成!\n`)
+    for (let key in TypeDirNameMap) {
+        shell.exec(`cd ${`packages/${TypeDirNameMap[key]}`} && ${buildCmd}`)
     }
-}
+    console.log(chalk.green`\n打包完成!\n`)
 
-const selectVersionType = () => {
-    return select({
-        name: 'versionType',
-        choices: [
-            { name: '主版本号', value: VersionTypeEnum.MAJOR },
-            { name: '次版本号', value: VersionTypeEnum.SECONDARY },
-            { name: '补丁版本号', value: VersionTypeEnum.PATCH }
-        ],
-        message: '请选择版本类型：',
-        default: []
-    }).catch(() => {})
+    // 特性版本
+    const versionType = VersionTypeEnum.PATCH
+
+    console.log(chalk.green`\n开始修改版本号\n`)
+
+    for (let key in TypeDirNameMap) {
+        await generateVersion(
+            versionType,
+            `../packages/${TypeDirNameMap[key]}/package.json`,
+            TypeDirNameMap[key]
+        )
+    }
+    console.log(chalk.green`\n修改完成!\n`)
+
+    commitCode()
 }
 
 const commitCode = () => {
     console.log(chalk.green`\n开始提交代码!\n`)
-
+    shell.exec(`git add . && git commit -m 'feat: update version' -n && git push`)
     console.log(chalk.green`\n提交完成!\n`)
 }
 
@@ -151,6 +76,7 @@ const generateVersion = async (versionType, pkgPath, pkgName) => {
 
     pkg.default.version = version
     console.log(chalk.blue(`\n${pkgName} 当前版本号：${initVersion} 修改为 ${version}`))
+    return version
 }
 
 run()

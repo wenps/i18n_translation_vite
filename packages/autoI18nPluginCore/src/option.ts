039a5b04e4ae7d14d2a4592d88b34680c9ee1568
wenps <2534491497@qq.com>
2025-02-14 10:02:41 +0800

    feat: 更新lodash使用方式

--- packages/autoI18nPluginCore/src/option.ts
+++ packages/autoI18nPluginCore/src/option.ts
@@ -1,15 +1,15 @@
 /*
  * @Author: xiaoshanwen
  * @Date: 2023-10-26 17:34:47
- * @LastEditTime: 2025-02-11 23:09:10
+ * @LastEditTime: 2025-02-14 09:52:55
  * @FilePath: /i18n_translation_vite/packages/autoI18nPluginCore/src/option.ts
  */
 
-import cloneDeep from 'lodash/cloneDeep'
 import { OriginLangKeyEnum } from './enums'
 import { YoudaoTranslator, GoogleTranslator, Translator, TranslatorOption } from './translator'
 export { YoudaoTranslator, GoogleTranslator, Translator }
 export type { TranslatorOption }
+const cloneDeep = require('lodash/cloneDeep')
 
 /**
  * 默认插件配置选项
